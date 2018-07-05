d627aa2c9475cc03b728ae9c9b24a9991c9c5100
Matas Šilinskas <silinskas.matas@gmail.com>
2018-07-05 16:40:01 +0300

    Apply PSR2

--- Psr4Autoloader.php
+++ Psr4Autoloader.php
@@ -22,7 +22,7 @@ class Psr4AutoloaderClass
      * Adds a base directory for a namespace prefix.
      *
      * @param string $prefix   The namespace prefix.
-     * @param string $base_dir A base directory for class files in the
+     * @param string $baseDir A base directory for class files in the
      *                         namespace.
      * @param bool   $prepend  If true, prepend the base directory to the stack
      *                         instead of appending it; this causes it to be
@@ -30,21 +30,17 @@ class Psr4AutoloaderClass
      *
      * @return void
      */
-    public function addNamespace($prefix, $base_dir, $prepend = false)
+    public function addNamespace($prefix, $baseDir)
     {
         $prefix = trim($prefix, '\\') . '\\';
 
-        $base_dir = rtrim($base_dir, DIRECTORY_SEPARATOR) . '/';
+        $baseDir = rtrim($baseDir, DIRECTORY_SEPARATOR) . '/';
 
         if (isset($this->prefixes[$prefix]) === false) {
             $this->prefixes[$prefix] = array();
         }
 
-        if ($prepend) {
-            array_unshift($this->prefixes[$prefix], $base_dir);
-        } else {
-            array_push($this->prefixes[$prefix], $base_dir);
-        }
+        array_push($this->prefixes[$prefix], $baseDir);
     }
 
     /**
@@ -60,14 +56,13 @@ class Psr4AutoloaderClass
         $prefix = $class;
 
         while (false !== $pos = strrpos($prefix, '\\')) {
-
             $prefix = substr($class, 0, $pos + 1);
 
-            $relative_class = substr($class, $pos + 1);
+            $relativeClass = substr($class, $pos + 1);
 
-            $mapped_file = $this->loadMappedFile($prefix, $relative_class);
-            if ($mapped_file) {
-                return $mapped_file;
+            $mappedFile = $this->loadMappedFile($prefix, $relativeClass);
+            if ($mappedFile) {
+                return $mappedFile;
             }
 
             $prefix = rtrim($prefix, '\\');
@@ -80,20 +75,19 @@ class Psr4AutoloaderClass
      * Load the mapped file for a namespace prefix and relative class.
      *
      * @param string $prefix         The namespace prefix.
-     * @param string $relative_class The relative class name.
+     * @param string $relativeClass The relative class name.
      *
      * @return bool
      */
-    protected function loadMappedFile(string $prefix, string $relative_class)
+    protected function loadMappedFile(string $prefix, string $relativeClass)
     {
         if (isset($this->prefixes[$prefix]) === false) {
             return false;
         }
 
-        foreach ($this->prefixes[$prefix] as $base_dir) {
-
-            $file = $base_dir
-                . str_replace('\\', '/', $relative_class)
+        foreach ($this->prefixes[$prefix] as $baseDir) {
+            $file = $baseDir
+                . str_replace('\\', '/', $relativeClass)
                 . '.php';
 
             if ($this->requireFile($file)) {
