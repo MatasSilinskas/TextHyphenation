9b4f49acfe0fc5a44708bdd3d2209d5b34e29fb1
Matas.Šilinskas <silinskas.matas@gmail.com>
2018-07-16 14:20:19 +0300

    Separate words and patterns REST logic

--- Executables/API.php
+++ Executables/API.php
@@ -20,18 +20,38 @@ class API implements ExecutableInterface
 
     public function execute()
     {
+        $table = end(explode('/', $_SERVER['REQUEST_URI']));
+        switch ($table) {
+            case 'patterns':
+                $this->executeForPatternsTable();
+                break;
+            case 'words':
+                $this->executeForWordsTable();
+                break;
+        }
+    }
+
+    public function executeForWordsTable()
+    {
         switch ($_SERVER['REQUEST_METHOD']) {
             case 'GET':
+                var_dump('aa');
                 echo json_encode($this->rest->getWords());
                 break;
+            case 'DELETE':
+                $this->rest->deleteWord();
+                break;
+        }
+    }
+
+    public function executeForPatternsTable()
+    {
+        switch ($_SERVER['REQUEST_METHOD']) {
             case 'POST':
                 $this->rest->addPattern();
                 break;
             case 'PUT':
                 $this->rest->updatePattern();
-                break;
-            case 'DELETE':
-                $this->rest->deleteWord();
                 break;
         }
     }
