25f4e44b029dec93d84c35ea827765520cfe830d
Matas.Šilinskas <silinskas.matas@gmail.com>
2018-07-20 16:49:00 +0300

    Implement CacheDecoratorTest

---
+++ tests/CacheDecoratorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace TextHyphenation\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+use TextHyphenation\Cache\ArrayCachePool;
+use TextHyphenation\Hyphenators\Cache;
+use TextHyphenation\Hyphenators\Hyphenator;
+
+class CacheDecoratorTest extends TestCase
+{
+    /**
+     * @throws \ReflectionException
+     * @throws \TextHyphenation\Cache\InvalidArgumentException
+     */
+    public function testHyphenate(): void
+    {
+        $hyphenator = $this->createMock(Hyphenator::class);
+        $hyphenator
+            ->expects($this->once())
+            ->method('hyphenate')
+            ->willReturn('nonCachedValue');
+
+        $cachePool = $this->createMock(ArrayCachePool::class);
+        $cachePool
+            ->expects($this->once())
+            ->method('get')
+            ->willReturn('cachedValue');
+
+        $cachePool
+            ->expects($this->once())
+            ->method('set');
+
+        $cachePool
+            ->method('has')
+            ->willReturnOnConsecutiveCalls(false, true);
+
+        $cache = new Cache($hyphenator, $cachePool);
+
+        $this->assertEquals('nonCachedValue', $cache->hyphenate(''));
+        $this->assertEquals('cachedValue', $cache->hyphenate(''));
+    }
+}
