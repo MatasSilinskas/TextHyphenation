bc1bc7aec9d07cfc3e9003cf43dc64febb7035d3
Matas.Šilinskas <silinskas.matas@gmail.com>
2018-07-20 10:10:33 +0300

    Add hyphenator tests

---
+++ tests/HyphenatorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use TextHyphenation\DataProviders\ProviderFactory;
+use TextHyphenation\Hyphenators\Hyphenator;
+
+class HyphenatorTest extends TestCase
+{
+    /**
+     * @param string $word
+     * @param string $expected
+     * @throws Exception
+     * 
+     * @dataProvider hyphenationProvider
+     */
+    public function testHyphenate(string $word, string $expected)
+    {
+        $factory = new ProviderFactory();
+        $patternsProvider = $factory->createProvider('patterns');
+        $hyphenator = new Hyphenator($patternsProvider->getData());
+        $this->assertEquals($expected, $hyphenator->hyphenate($word));
+    }
+
+    public function hyphenationProvider()
+    {
+        return [
+            ['mistranslate', 'mis-trans-late'],
+            ['alphabetical', 'al-pha-bet-i-cal'],
+            ['bewildering', 'be-wil-der-ing'],
+            ['buttons', 'but-ton-s'],
+            ['ceremony', 'cer-e-mo-ny'],
+            ['hovercraft', 'hov-er-craft'],
+            ['lexicographically', 'lex-i-co-graph-i-cal-ly'],
+            ['programmer', 'pro-gram-mer'],
+            ['recursion', 're-cur-sion'],
+            ['aa', 'aa'],
+        ];
+    }
+}
