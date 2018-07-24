e9f6c65be7921f02f6d72374ed7fd3e0478a415c
Matas.Šilinskas <silinskas.matas@gmail.com>
2018-07-24 12:40:22 +0300

    Implement simple delete functionality

--- views/js/getPatterns.js
+++ views/js/getPatterns.js
@@ -1,20 +1,23 @@
 let bio = document.getElementById('patterns');
-
 let request = new XMLHttpRequest();
 
 request.onreadystatechange = function() {
-    if(request.readyState === 4) {
+    if (request.readyState === 4) {
         if (this.readyState === 4 && this.status === 200) {
-            myObj = JSON.parse(this.responseText);
+            let myObj = JSON.parse(this.responseText);
             let txt = '<table class="table table-hover table-bordered">' +
                 "<tr>" +
                 "<th>Id</th>" +
                 "<th>Pattern</th>" +
+                "<th>Delete</th>" +
                 "</tr>";
             for (let row in myObj) {
                 txt +=
                     `<tr><td>${myObj[row].id}</td>` +
-                    `<td>${myObj[row].pattern}</td></tr>`;
+                    `<td>${myObj[row].pattern}</td>` +
+                    `<td><button type="button" class="btn btn-danger" value="${myObj[row].pattern}" ` +
+                    `onclick="deletePattern(this.value)">` + `Delete</button></td>` +
+                    `</tr>`;
             }
             txt += "</table>";
             bio.innerHTML = txt;
@@ -24,3 +27,19 @@ request.onreadystatechange = function() {
 
 request.open('Get', 'api/patterns');
 request.send();
+
+function deletePattern(pattern){
+    let deleteRequest = new XMLHttpRequest();
+    deleteRequest.onload = function() {
+        if (deleteRequest.status === 200) {
+            location.reload();
+            alert('Delete was successful!');
+        } else {
+            alert('Something went wrong');
+        }
+    };
+    deleteRequest.open('Delete', 'api/patterns');
+    let data = {pattern:pattern};
+    deleteRequest.send(JSON.stringify(data));
+}
+
--- views/js/getWords.js
+++ views/js/getWords.js
@@ -1,11 +1,10 @@
 let bio = document.getElementById('words');
-
 let request = new XMLHttpRequest();
 
 request.onreadystatechange = function() {
     if(request.readyState === 4) {
         if (this.readyState === 4 && this.status === 200) {
-            myObj = JSON.parse(this.responseText);
+            let myObj = JSON.parse(this.responseText);
             let txt = '<table class="table table-hover table-bordered">' +
                 "<tr>" +
                 "<th>Id</th>" +
@@ -16,7 +15,10 @@ request.onreadystatechange = function() {
                 txt +=
                     `<tr><td>${myObj[row].id}</td>` +
                     `<td>${myObj[row].word}</td>` +
-                    `<td>${myObj[row].hyphenated}</td></tr>`;
+                    `<td>${myObj[row].hyphenated}</td>` +
+                    `<td><button type="button" class="btn btn-danger" value="${myObj[row].word}" ` +
+                    `onclick="deleteWord(this.value)">` + `Delete</button></td>` +
+                    `</tr>`;
             }
             txt += "</table>";
             bio.innerHTML = txt;
@@ -26,3 +28,18 @@ request.onreadystatechange = function() {
 
 request.open('Get', 'api/words');
 request.send();
+
+function deleteWord(word){
+    let deleteRequest = new XMLHttpRequest();
+    deleteRequest.onload = function() {
+        if (deleteRequest.status === 200) {
+            location.reload();
+            alert('Delete was successful!');
+        } else {
+            alert('Something went wrong');
+        }
+    };
+    deleteRequest.open('Delete', 'api/words');
+    let data = {word:word};
+    deleteRequest.send(JSON.stringify(data));
+}
